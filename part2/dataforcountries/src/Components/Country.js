07ac6c42c0bc926d0558492a86d3847389792cb4
Avensi <helenali2000@hotmail.com>
2023-06-01 12:59:33 +0200

    Exercice 2.19

--- part2/dataforcountries/src/Components/Country.js
+++ part2/dataforcountries/src/Components/Country.js
@@ -1,6 +1,19 @@
+import { useState } from "react"
+import CountryDetail from "./CountryDetail"
+
 const Country = ({country}) => {
+
+    const [showDetail, setShowDetail] = useState(false)
+
+    const handleShow = () => {
+        setShowDetail(!showDetail)
+    }
+
     return (
-        <p>{country.name.common}</p>
+        <>
+        <p>{country.name.common} <button onClick={handleShow}>{showDetail ? "hide" : "show"}</button></p>
+        {showDetail ? <CountryDetail country={country}/> : null}
+        </>
     )
 }
 
