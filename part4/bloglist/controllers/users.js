d52969691122194fd61cfdae17d2e7872ed30acb
Helena Li <helenali2000@hotmail.com>
2023-07-21 23:17:12 +0200

    Exercice 4.15 added a GET

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -2,6 +2,13 @@ const userRouter = require('express').Router()
 const User = require('../models/user')
 const bcryptjs = require('bcryptjs')
 
+
+userRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+
+})
+
 userRouter.post('/', async (request, response) => {
   const {username, name, password} = request.body
 
