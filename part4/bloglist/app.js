09892d051e9b3bc0af957637202803ff0e2e36e3
Helena Li <helenali2000@hotmail.com>
2023-07-23 20:08:04 +0200

    Exercice 4.18 + 4.19

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
@@ -18,7 +19,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs',blogRouter)
 app.use('/api/users',userRouter)
-
+app.use('/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,7 +1,15 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {username : 1, name : 1, id :1})
@@ -11,7 +19,11 @@ blogRouter.get('/', async (request, response) => {
   
 blogRouter.post('/', async (request, response) => {
 
-  const user = await User.findById(request.body.user)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     author : request.body.author,
