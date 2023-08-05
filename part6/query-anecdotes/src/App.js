7df43e41b25ef66389a60dfba122556c223d68c7
Helena Li <helenali2000@hotmail.com>
2023-08-05 19:13:49 +0200

    Exercice 6.22

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,19 +1,25 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery} from 'react-query'
-import {getAnecdotes} from './services/requests'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { getAnecdotes, updateAnecdote } from './services/requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')}
+    })
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    console.log({...anecdote, votes : anecdote.votes + 1})
+    updateAnecdoteMutation.mutate({...anecdote, votes : anecdote.votes + 1})
   }
 
   const result = useQuery('anecdotes',getAnecdotes, {
     retry: 1,
     refetchOnWindowFocus: false
   })
-
   
   if ( result.isLoading ) {
     return <div>loading data...</div>
--- part6/query-anecdotes/src/services/requests.js
+++ part6/query-anecdotes/src/services/requests.js
@@ -2,5 +2,13 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/anecdotes'
 
-export const getAnecdotes = () =>
-  axios.get(baseUrl).then(res => res.data)
+export const getAnecdotes = () => {
+  return axios.get(baseUrl).then(res => res.data)
+}
+
+export const createAnecdote = (anecdote) => {
+  return axios.post(baseUrl, anecdote).then(res => res.data)
+}
+
+export const updateAnecdote = updatedAnecdote =>
+  axios.put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote).then(res => res.data)
